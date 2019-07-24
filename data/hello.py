1178b242b643db094bea24d0c0b4b6caaecc99e5
lif54334 <lif54334@163.com>
2019-07-24 18:35:28 +0800

    first

---
+++ data/hello.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# -*- encoding: utf-8 -*-
+
+'''
+@Author  :   {lif54334}
+
+@Software:   PyCharm
+
+@File    :   hello.py
+
+@Time    :   2019/7/24 17:57
+
+@Desc    :
+
+'''
+
+pass
